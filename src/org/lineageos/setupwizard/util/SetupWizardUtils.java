b0fd02990ab00fe474fd286dfd11048374aeff37
Sava Mikalacki <mikalackis@gmail.com>
2022-03-22 12:04:12 +0100

    Ariel: Simple preparation for beta

--- src/org/lineageos/setupwizard/util/SetupWizardUtils.java
+++ src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -65,6 +65,8 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import lineageos.providers.LineageSettings;
+
 public class SetupWizardUtils {
 
     private static final String TAG = SetupWizardUtils.class.getSimpleName();
@@ -223,6 +225,14 @@ public class SetupWizardUtils {
             Settings.Secure.putInt(contentResolver,
                     Settings.Secure.TV_USER_SETUP_COMPLETE, 1);
         }
+        // ARIEL MOD
+        Settings.Global.putString(contentResolver,
+                Settings.Global.PRIVATE_DNS_MODE, "hostname");
+        Settings.Global.putString(contentResolver,
+                Settings.Global.PRIVATE_DNS_SPECIFIER, "dns.adguard.com");
+        LineageSettings.Secure.putInt(contentResolver,
+                LineageSettings.Secure.STATS_COLLECTION, 0);
+        // ARIEL MOD
 
         disableComponent(context, WizardManager.class);
         disableHome(context);
