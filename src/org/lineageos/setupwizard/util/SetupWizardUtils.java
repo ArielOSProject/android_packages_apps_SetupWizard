0234426ffda01195512dd0274a511817c708d80b
Sava Mikalacki <mikalackis@gmail.com>
2025-08-29 09:57:44 +0200

    Remove custom dns setup

--- src/org/lineageos/setupwizard/util/SetupWizardUtils.java
+++ src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -231,14 +231,6 @@ public class SetupWizardUtils {
             Settings.Secure.putInt(contentResolver,
                     Settings.Secure.TV_USER_SETUP_COMPLETE, 1);
         }
-        // ARIEL MOD
-        Settings.Global.putString(contentResolver,
-                Settings.Global.PRIVATE_DNS_MODE, "hostname");
-        Settings.Global.putString(contentResolver,
-                Settings.Global.PRIVATE_DNS_SPECIFIER, "dns.adguard.com");
-        LineageSettings.Secure.putInt(contentResolver,
-                LineageSettings.Secure.STATS_COLLECTION, 0);
-        // ARIEL MOD
 
         disableComponent(context, WizardManager.class);
         disableHome(context);
